fa5bf059264312e60c155cdc0eedcd79b7962270
teiguell <79284345+teiguell@users.noreply.github.com>
2025-06-02 17:41:20 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,14 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import App from "./App";
+import SimpleApp from "./SimpleApp";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
-
 if (rootElement) {
   const reactRoot = createRoot(rootElement);
   reactRoot.render(
     <React.StrictMode>
-      <App />
+      <SimpleApp />
     </React.StrictMode>
   );
 } else {
