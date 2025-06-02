5f098ea88c31f04f2f71ad573de363ba6907ed8a
teiguell <79284345+teiguell@users.noreply.github.com>
2025-06-02 17:19:48 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,10 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
+import { cartographer } from "@replit/vite-plugin-cartographer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +12,9 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   plugins: [
     react(),
+    runtimeErrorOverlay(),
+    themePlugin(),
+    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined ? [cartographer()] : []),
   ],
   resolve: {
     alias: {
